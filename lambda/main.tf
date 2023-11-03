99020d3f2eb1ec3d33109a84639cf28110458c4a
Nikodem Pietrzak <nikodem4799@gmail.com>
2023-11-03 16:06:29 +0100

    fix_small_error

--- lambda/main.tf
+++ lambda/main.tf
@@ -36,7 +36,7 @@ resource "aws_cloudwatch_event_rule" "daily_lambda_trigger" {
 resource "aws_cloudwatch_event_target" "terraform_lambda_every_day" {
     rule = aws_cloudwatch_event_rule.daily_lambda_trigger.name
     target_id = "get_data_lambda"
-    arn = aws_lambda_function.test_lambda.arn
+    arn = aws_lambda_function.get_data_lambda.arn
 }
 
 resource "aws_lambda_permission" "allow_event_to_trigger_lambda" {
