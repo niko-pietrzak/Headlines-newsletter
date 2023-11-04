5105d762e972720cd1bb45209654d49d948ba640
Nikodem Pietrzak <nikodem4799@gmail.com>
2023-11-04 12:27:13 +0100

    added_policies_for_second_lambda

--- iam/main.tf
+++ iam/main.tf
@@ -24,7 +24,7 @@ resource "aws_iam_role" "iam_for_news_lambda" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-# 3. Defining json policies for lambda
+# 3. Defining json policies for lambdas
 data "aws_iam_policy_document" "lambda_logging" {
   statement {
     effect = "Allow"
@@ -53,6 +53,31 @@ data "aws_iam_policy_document" "lambda_s3_write" {
   }
 }
 
+data "aws_iam_policy_document" "lambda_s3_read" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+                "s3:GetObject",
+                "s3:ListBucket"
+    ]
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "lambda_ses_send" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+                "ses:SendEmail",
+                "ses:SendRawEmail"
+    ]
+    resources = ["*"]
+  }
+}
+
+
 # 4. Assigning policy definitions to policy names
 resource "aws_iam_policy" "lambda_logging" {
   name        = "lambda_logging"
@@ -68,6 +93,20 @@ resource "aws_iam_policy" "lambda_s3_write" {
   policy      = data.aws_iam_policy_document.lambda_s3_write.json
 }
 
+resource "aws_iam_policy" "lambda_s3_read" {
+  name        = "lambda_s3_read"
+  path        = "/"
+  description = "IAM policy for S3 read access for a lambda"
+  policy      = data.aws_iam_policy_document.lambda_s3_read.json
+}
+
+resource "aws_iam_policy" "lambda_ses_send" {
+  name        = "lambda_ses_send"
+  path        = "/"
+  description = "IAM policy for lambda to send email via SES"
+  policy      = data.aws_iam_policy_document.lambda_ses_send.json
+}
+
 # 5. Assigning policies to roles
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
   role       = aws_iam_role.iam_for_lambda.name
@@ -77,4 +116,19 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
 resource "aws_iam_role_policy_attachment" "lambda_s3_writes" {
   role       = aws_iam_role.iam_for_lambda.name
   policy_arn = aws_iam_policy.lambda_s3_write.arn
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_s3_writes" {
+  role       = aws_iam_role.iam_for_news_lambda.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_s3_writes" {
+  role       = aws_iam_role.iam_for_news_lambda.name
+  policy_arn = aws_iam_policy.lambda_s3_read.arn
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_s3_writes" {
+  role       = aws_iam_role.iam_for_news_lambda.name
+  policy_arn = aws_iam_policy.lambda_ses_send.arn
 }
